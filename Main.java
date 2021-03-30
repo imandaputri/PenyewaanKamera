438bcb47654659ff7beb8faf45dba4f12d5e667b
MegaSiraturahmi22 <64682133+MegaSiraturahmi22@users.noreply.github.com>
2021-03-30 16:17:53 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (Hijaiyah, Mega, Safratul)
+ * @version (30 Maret 2021)
+ */
+public class Main
+{
+     public static void main (String args[]){
+         DSLR kameraDSLR = new DSLR();
+         MirrorLens kameraMirrorLens = new MirrorLens();
+         
+         SewaKamera kamera1 = new SewaKamera();
+         SewaKamera kamera2 = new SewaKamera();
+         
+         kamera1.Konfirmasi(kameraDSLR);
+        // kamera2.Konfirmasi(kameraMirrorLens);
+         
+         kamera1.Transaksi();
+        // kamera2.Transaksi();
+         
+        }
+}
